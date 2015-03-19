fd09de12565f501ec76c4a5fc2d340050db11084
Dan Wilhelm <wil12045@byui.edu>
2015-03-19 11:07:09 -0700

    Revamped Login system using HTML/Javascript/Ajax

--- Team_4_W15_CIT261/public_html/MyScheduler/login/checkform.php
+++
@@ -1,9 +0,0 @@
-<?php
-        
-     if ($_POST['action']=='Send'){
-         $username = $_POST['username'];
-         $password = $_POST['password'];
-         include 'form.php';
-     }
-
-?>
--- Team_4_W15_CIT261/public_html/MyScheduler/login/form.php
+++
@@ -1,8 +0,0 @@
-<form name="login" action="/login/login.php" id="loginform">
-    <fieldset>
-    <legend>All fields are required.</legend>
-        <label for="username">User Name:</label><input type="text" name="username" id="username" size="8" value="<?php echo $username; ?>" required>
-        <label for="password">Email:</label><input type="text" name="password" id="email" size="45"  value="<?php echo $password; ?>" required>
-        <label for="action" id="actionLabel">&nbsp;</label><input type="submit" name="action" id="action" value="Send">
-    </fieldset>
-</form>
