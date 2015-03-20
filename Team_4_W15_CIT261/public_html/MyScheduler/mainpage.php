a11ed6e82426c15932ed9ebe2b630d54b84b1031
Dan Wilhelm <wil12045@byui.edu>
2015-03-20 16:41:22 -0700

    Working Login and Registration

---
+++ Team_4_W15_CIT261/public_html/MyScheduler/mainpage.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>My Scheduler</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h1>Temporary Page</h1>
+    </body>
+</html>
